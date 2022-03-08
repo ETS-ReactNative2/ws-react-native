73870a499f8468745e9cb611843a42cfd974db29
AlineBsr <bosser.aline@gmail.com>
2022-03-08 11:06:44 +0100

    work2

--- components/Auth/ProfileScreen.jsx
+++ components/Auth/ProfileScreen.jsx
@@ -22,31 +22,78 @@ const ProfileScreen = ({ navigation }) => {
 
   // WORKSHOP TODO : CODE HERE
   return (
-    <View >
-
-        <View  >
-          <Text >Profil</Text>
+    <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.navSetting}>
+          <Text style={styles.title}>Profil</Text>
         </View>
         
         {/* Display Username of user */}
+        <Text style={styles.h2}>Nom</Text>
+        <Text style={styles.text}>{auth?.currentUser?.displayName}</Text>
 
         {/* Display Email of user */}
+        <Text style={styles.h2}>Email</Text>
 
+        <Text style={styles.text}>{auth?.currentUser?.email}</Text>
         {/*
           Display a Button for
           - go to the shoppping lists 
           - user loggout 
         */}
-       
+        <Button onPress={() => navigation.navigate("Vos listes de courses")}
+          color={Colors.darkGreen}
+          backgroundColor={Colors.white}
+          title='Vers vos listes' 
+        />
+        <Button
+          title='Déconnexion'
+          color={Colors.red}
+          backgroundColor={Colors.white}
+          onPress={logOut}
+        />
         <StatusBar style="auto" translucent={true} />
-
+      </ImageBackground>
     </View>
   );
 }
 
-// WORKSHOP TODO : DEFINE YOUR STYLE
-
-
+// STYLE
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: Colors.darkGrey,
+    fontWeight: '700',
+    fontSize: 60,
+    textAlign: 'center',
+    marginBottom: 60,
+  },
+  h2: {
+    color: Colors.darkGrey70,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 0,
+    marginBottom: 10,
+    width: 240,
+  },
+  text: {
+    color: Colors.darkGrey,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 0,
+    marginBottom: 40,
+    width: 240,
+  }
+});
 
 // EXPORT
 export default ProfileScreen;
